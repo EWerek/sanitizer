6b62c032023c121d44a559e3b0151d587626ef27
Jamie Sokolsky <jamiesokolsky@jamies-mbp.home>
2020-06-14 22:47:03 +0100

    added google analytics

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import LoadScript from 'vue-plugin-load-script';
 import router from "./router";
+import VueAnalytics from 'vue-analytics';
+
 
 Vue.use(LoadScript);
+Vue.use(VueAnalytics, {
+  id: 'UA-169380404-1',
+  router
+});
 
 
 
